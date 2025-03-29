fe3e21f01bf04cc6480bbd6abff4b914d84f6975
Gabriel Lee <gabewblee@Mac.lan>
2025-03-29 03:41:58 -0400

    Beginning to add Field

---
+++ Backend/BattleField.cpp
@@ -0,0 +1,14 @@
+class Field {
+public: 
+    int width;
+    int height;
+
+    Field(int width, int height) {
+        this->width = width;
+        this->height = height;
+    }
+
+    int makeField(int fieldNumber) {
+        ;
+    }
+};
