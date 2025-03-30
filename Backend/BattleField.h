f0848cc05907dbde12f4bc502a7a55f51071f5e3
Gabriel Lee <gabewblee@Mac.lan>
2025-03-29 21:51:55 -0400

    Basic structure of the Battle Field added

---
+++ Backend/BattleField.h
@@ -0,0 +1,43 @@
+#ifndef FIELD
+#define FIELD
+
+#include <vector>
+#include "Tile.h"
+
+std::vector<std::vector<int>> fieldOne = {
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0}
+};
+
+std::vector<std::vector<int>> fieldTwo = {
+    {0, 0, 2, 2, 0, 2, 2, 0, 2, 2, 0, 0},
+    {0, 0, 2, 2, 0, 2, 2, 0, 2, 2, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+};
+
+std::vector<std::vector<std::vector<int>>> fields = {fieldOne, fieldTwo};
+
+class Field {
+public:
+    int fieldNumber;
+    std::vector<std::vector<int>> field;
+
+    Field(int fieldNumber) : fieldNumber(fieldNumber) {
+        this->field = fields[fieldNumber];
+    }
+};
+
+#endif
