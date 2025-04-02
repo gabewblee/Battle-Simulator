789c1162c7bc48bcf70f40304dfc6b776b35c4ef
Gabriel Lee <gabewblee@Mac.lan>
2025-04-01 20:13:30 -0400

    Changed Name of directory

--- Frontend/Window Manager/ScreenManager.cpp
+++
@@ -1,8 +0,0 @@
-#ifndef MANAGER
-#define MANAGER
-
-#include "ScreenManager.h"
-
-ScreenManager::ScreenManager(unsigned int width, unsigned int height) : window(sf::VideoMode({width, height}), "Battle Simulator") {}
-
-#endif 
--- Frontend/Window Manager/ScreenManager.h
+++
@@ -1,8 +0,0 @@
-#include <SFML/Graphics.hpp>
-#include <string>
-
-class ScreenManager {
-public:
-    sf::RenderWindow window;
-    ScreenManager(unsigned int width, unsigned int height);
-};
